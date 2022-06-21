c1ff20750ad6dba43ff173097e5d507ab0614b49
NsikanPatrick <nsikanpatrick69@gmail.com>
2022-06-21 11:30:55 +0200

    Welcome

---
+++ main.js
@@ -0,0 +1,6 @@
+function welcome(name, height, country) {
+    console.log("Hello, my name is "+name+", I am " +height+ 
+    " inches tall, and i am a "+country+".");
+  }
+  
+  welcome("Nsikan Patrick", 6.2, "Nigerian");
